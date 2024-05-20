db80b7ad568ac1d5f74714b97b89ec0693269542
Sarah Clark <sarahireneclark@gmail.com>
2024-05-20 13:55:07 -0500

    Initial commit of FirebaseApp project

---
+++ FirebaseApp/FirebaseApp/FirebaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAppApp.swift
+//  FirebaseApp
+//
+//  Created by Sarah Clark on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
